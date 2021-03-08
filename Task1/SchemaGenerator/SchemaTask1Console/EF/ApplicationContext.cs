7a9188fbb375c6b474e073e7762bdadcecf28274
Kamil Kowalewski <szamil24@gmail.com>
2021-03-08 14:29:08 +0100

    Add DbSets

--- Task1/SchemaGenerator/SchemaTask1Console/EF/ApplicationContext.cs
+++ Task1/SchemaGenerator/SchemaTask1Console/EF/ApplicationContext.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SchemaTask1Console.Models;
 
 namespace SchemaTask1Console.EF {
 
     public class ApplicationContext : DbContext {
 
         /*------------------------ FIELDS REGION ------------------------*/
-        /*TODO ADD DbSets*/
+        public DbSet<Employee> Employees { get; private set; }
+        public DbSet<Grocery> Groceries { get; private set; }
+        public DbSet<Product> Products { get; private set; }
+        public DbSet<HistoricalProduct> HistoricalProducts { get; private set; }
+        public DbSet<Supplier> Suppliers { get; private set; }
 
         /*------------------------ METHODS REGION ------------------------*/
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
