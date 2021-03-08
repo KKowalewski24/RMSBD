ea0a9268beb373450863aeb68fcdaf3abc4716ed
Kamil Kowalewski <szamil24@gmail.com>
2021-03-08 17:43:15 +0100

    Change hierarchy of classes

--- Task1/SchemaGenerator/SchemaTask1Console/Models/HistoricalProduct.cs
+++ Task1/SchemaGenerator/SchemaTask1Console/Models/HistoricalProduct.cs
@@ -2,9 +2,11 @@
 
 namespace SchemaTask1Console.Models {
 
-    public class HistoricalProduct : Product {
+    public class HistoricalProduct : BaseEntity {
 
         /*------------------------ FIELDS REGION ------------------------*/
+        public string Name { get; private set; }
+        public float Amount { get; private set; }
         public decimal Price { get; private set; }
         public DateTime TransactionDate { get; private set; }
 
@@ -12,14 +14,18 @@ namespace SchemaTask1Console.Models {
         protected HistoricalProduct() {
         }
 
-        public HistoricalProduct(string name, float amount, decimal price, DateTime transactionDate)
-            : base(name, amount) {
+        public HistoricalProduct(string name, float amount, decimal price,
+                                 DateTime transactionDate) {
+            Name = name;
+            Amount = amount;
             Price = price;
             TransactionDate = transactionDate;
         }
 
         public override string ToString() {
             return $"{base.ToString()}, " +
+                   $"{nameof(Name)}: {Name}, " +
+                   $"{nameof(Amount)}: {Amount}, " +
                    $"{nameof(Price)}: {Price}, " +
                    $"{nameof(TransactionDate)}: {TransactionDate}";
         }
