27b680840d78b68ebddae66198cc5ec41332faec
Kamil Kowalewski <szamil24@gmail.com>
2021-03-09 11:41:02 +0100

    Add procedures

--- Task1/0-Create-DB.sql
+++ Task1/0-Create-DB.sql
@@ -2,3 +2,35 @@
     WITH OWNER postgres;
 
 DROP DATABASE groceryshopsdb;
+
+---------------------------------------
+SELECT *
+FROM employees;
+
+SELECT *
+FROM suppliers;
+
+SELECT *
+FROM historicalproducts;
+
+SELECT *
+FROM products;
+
+SELECT *
+FROM groceries;
+---------------------------------------
+DELETE
+FROM suppliers;
+
+DELETE
+FROM employees;
+
+DELETE
+FROM historicalproducts;
+
+DELETE
+FROM products;
+
+DELETE
+FROM groceries;
+---------------------------------------
--- Task1/3-Basic-Functions.sql
+++ Task1/3-Basic-Functions.sql
@@ -1,35 +1,5 @@
-SELECT *
-FROM employees;
-
-SELECT *
-FROM suppliers;
-
-SELECT *
-FROM historicalproducts;
-
-SELECT *
-FROM products;
-
-SELECT *
-FROM groceries;
----------------------------------------
-DELETE
-FROM suppliers;
-
-DELETE
-FROM employees;
-
-DELETE
-FROM historicalproducts;
-
-DELETE
-FROM products;
-
-DELETE
-FROM groceries;
----------------------------------------
-CREATE OR REPLACE PROCEDURE add_products(product_name TEXT, product_amount INTEGER,
-                                         grocery_id INTEGER, supplier_id INTEGER)
+CREATE OR REPLACE PROCEDURE add_product(product_name TEXT, product_amount INTEGER,
+                                        grocery_id INTEGER, supplier_id INTEGER)
     LANGUAGE SQL
 AS
 $$
@@ -37,5 +7,44 @@ INSERT INTO products (name, amount, groceryid, supplierid)
 VALUES (product_name, product_amount, grocery_id, supplier_id);
 $$;
 
-CALL add_products('Banana', 20, 1, 1);
+CALL add_product('Banana', 20, 1, 1);
+
+
+---------------------------------------
+
+CREATE OR REPLACE PROCEDURE add_employee(first_name TEXT, last_name TEXT, salary NUMERIC)
+    LANGUAGE SQL
+AS
+$$
+INSERT INTO employees (firstname, lastname, salary)
+VALUES (first_name, last_name, salary);
+$$;
+
+CALL add_employee('Adam', 'Nowak', 3000);
+
+---------------------------------------
+
+CREATE OR REPLACE PROCEDURE add_supplier(name TEXT, gross_vehicle_weight REAL,
+                                         employee_id INTEGER)
+    LANGUAGE SQL
+AS
+$$
+INSERT INTO suppliers (name, grossvehicleweight, employeeid)
+VALUES (name, gross_vehicle_weight, employee_id);
+$$;
+
+CALL add_supplier('Third supplier', 2500, 2);
+
+---------------------------------------
+
+CREATE OR REPLACE PROCEDURE add_grocery(grocery_address TEXT, supplier_id INTEGER)
+    LANGUAGE SQL
+AS
+$$
+INSERT INTO groceries (address, supplierid)
+VALUES (grocery_address, supplier_id);
+$$;
+
+CALL add_grocery('Politechniki 25', 2)
+
 
