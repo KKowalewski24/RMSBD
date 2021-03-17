5d9700ecb8a568bba9620d9a72767d0062654f90
Kamil Kowalewski <szamil24@gmail.com>
2021-03-17 10:53:17 +0100

    Refactor of 2-Import-Export.sql

--- Task3/2-Import-Export.sql
+++ Task3/2-Import-Export.sql
@@ -1,32 +1,33 @@
-CREATE OR REPLACE FUNCTION xml_import(filename TEXT)
-    RETURNS XML
-    VOLATILE
-    LANGUAGE plpgsql AS
-$f$
-DECLARE
-    content BYTEA;
-    loid    OID;
-    lfd     INTEGER;
-    lsize   INTEGER;
+--
+-- The absolute path name of the input or output file. Windows users
+-- might need to use an E'' string and double any backslashes
+-- used in the path name.
+--
+
+CREATE OR REPLACE PROCEDURE xml_import()
+    LANGUAGE plpgsql
+AS
+$$
 BEGIN
-    loid := lo_import(filename);
-    lfd := lo_open(loid, 262144);
-    lsize := lo_lseek(lfd, 0, 2);
-    PERFORM lo_lseek(lfd, 0, 0);
-    content := loread(lfd, lsize);
-    PERFORM lo_close(lfd);
-    PERFORM lo_unlink(loid);
+    CREATE TEMPORARY TABLE IF NOT EXISTS temp_content (
+        content XML
+    );
 
-    RETURN xmlparse(DOCUMENT convert_from(content, 'UTF8'));
+    COPY temp_content FROM 'C:\Coding\RMSBD\Task3\car-showroom-minified.xml';
+
+    INSERT INTO car_showroom (xml_data)
+    SELECT *
+    FROM temp_content;
+
+    DROP TABLE temp_content;
 END;
-$f$;
+$$;
 
-INSERT INTO car_showroom (xml_data)
-VALUES (xml_import('C:\Coding\RMSBD\Task3\car-showroom-minified.xml'));
+CALL xml_import();
 
 ---------------------------------------
 
-CREATE OR REPLACE PROCEDURE xml_export_new()
+CREATE OR REPLACE PROCEDURE xml_export()
     LANGUAGE plpgsql
 AS
 $$
@@ -35,4 +36,32 @@ BEGIN
 END;
 $$;
 
-CALL xml_export_new()
+CALL xml_export();
+
+---------------------------------------
+-- DO NOT DELETE THIS
+-- CREATE OR REPLACE FUNCTION xml_import_low_level(filename TEXT)
+--     RETURNS XML
+--     VOLATILE
+--     LANGUAGE plpgsql AS
+-- $f$
+-- DECLARE
+--     content BYTEA;
+--     loid    OID;
+--     lfd     INTEGER;
+--     lsize   INTEGER;
+-- BEGIN
+--     loid := lo_import(filename);
+--     lfd := lo_open(loid, 262144);
+--     lsize := lo_lseek(lfd, 0, 2);
+--     PERFORM lo_lseek(lfd, 0, 0);
+--     content := loread(lfd, lsize);
+--     PERFORM lo_close(lfd);
+--     PERFORM lo_unlink(loid);
+--
+--     RETURN xmlparse(DOCUMENT convert_from(content, 'UTF8'));
+-- END;
+-- $f$;
+--
+-- INSERT INTO car_showroom (xml_data)
+-- VALUES (xml_import_low_level('C:\Coding\RMSBD\Task3\car-showroom-minified.xml'));
