9b023c561d523c78685d989deefcd82aa55011f7
Kamil Kowalewski <szamil24@gmail.com>
2021-03-17 19:24:05 +0100

    Change xml files + add selects for xml data type

--- Task3/4-View-Functions.sql
+++ Task3/4-View-Functions.sql
@@ -1,11 +1,36 @@
+-- https://www.2ndquadrant.com/en/blog/xmltable-intro/
+-- https://www.depesz.com/2017/03/09/waiting-for-postgresql-10-support-xmltable-query-expression/
+
 SELECT xml_data
 FROM car_showroom_single_column;
 
-SELECT xpath('car_showroom/brands/brand/text()', xml_data)
-FROM car_showroom_single_column;
+SELECT xmltable.*
+FROM car_showroom_single_column,
+    XMLTABLE('/car_showroom/brands/brand' PASSING xml_data
+             COLUMNS
+                 name TEXT
+        );
 
-SELECT xpath('car_showroom/vehicle_types/vehicle_type/text()', xml_data)
-FROM car_showroom_single_column;
+SELECT xmltable.*
+FROM car_showroom_single_column,
+    XMLTABLE('/car_showroom/vehicle_types/vehicle_type' PASSING xml_data
+             COLUMNS
+                 name TEXT
+        );
 
-SELECT xpath('car_showroom/engine_types/engine_type/text()', xml_data)
-FROM car_showroom_single_column;
+SELECT xmltable.*
+FROM car_showroom_single_column,
+    XMLTABLE('/car_showroom/engine_types/engine_type' PASSING xml_data
+             COLUMNS
+                 name TEXT
+        );
+
+-- Display Cars
+SELECT xmltable.*
+FROM car_showroom_single_column,
+    XMLTABLE('/car_showroom/cars/car' PASSING xml_data
+             COLUMNS
+                 model TEXT,
+                 production_year TEXT,
+                 price TEXT
+        );
