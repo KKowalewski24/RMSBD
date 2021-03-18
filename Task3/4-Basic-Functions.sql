6191ba7b381fbb267ca048d9b7b483f43a7593aa
Kamil Kowalewski <szamil24@gmail.com>
2021-03-18 23:19:03 +0100

    Move function to different file

--- Task3/4-Basic-Functions.sql
+++ Task3/4-Basic-Functions.sql
@@ -108,3 +108,55 @@ $$;
 
 SELECT *
 FROM get_all_cars();
+
+
+-- Display All Cars - fetch all data
+CREATE OR REPLACE FUNCTION get_all_data()
+    RETURNS TABLE (
+        brand_name        TEXT,
+        vehicle_type_name TEXT,
+        engine_type_name  TEXT,
+        model             TEXT,
+        production_year   TEXT,
+        price             TEXT
+    )
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    RETURN QUERY SELECT brand.brand_name, vehicle_type.vehicle_type_name,
+                        engine_type.engine_type_name, car.model, car.production_year,
+                        car.price
+                 FROM car_showroom_single_column,
+                      XMLTABLE('/car_showroom/brands/brand' PASSING xml_data
+                               COLUMNS
+                                   brand_id TEXT PATH '@brand_id' NOT NULL ,
+                                   brand_name TEXT PATH '.' NOT NULL
+                          ) brand,
+                      XMLTABLE('/car_showroom/vehicle_types/vehicle_type' PASSING xml_data
+                               COLUMNS
+                                   vehicle_type_id TEXT PATH '@vehicle_type_id' NOT NULL,
+                                   vehicle_type_name TEXT PATH '.' NOT NULL
+                          ) vehicle_type,
+                      XMLTABLE('/car_showroom/engine_types/engine_type' PASSING xml_data
+                               COLUMNS
+                                   engine_id TEXT PATH '@engine_id' NOT NULL,
+                                   engine_type_name TEXT PATH '.' NOT NULL
+                          ) engine_type,
+                      XMLTABLE('/car_showroom/cars/car' PASSING xml_data
+                               COLUMNS
+                                   brand_id TEXT PATH '@brand_id' NOT NULL,
+                                   vehicle_type_id TEXT PATH '@vehicle_type_id' NOT NULL,
+                                   engine_id TEXT PATH '@engine_id' NOT NULL,
+                                   model TEXT PATH 'model' NOT NULL,
+                                   production_year TEXT PATH 'production_year' NOT NULL,
+                                   price TEXT PATH 'price' NOT NULL
+                          ) car
+                 WHERE car.brand_id = brand.brand_id
+                   AND car.vehicle_type_id = vehicle_type.vehicle_type_id
+                   AND car.engine_id = engine_type.engine_id;
+END;
+$$;
+
+SELECT *
+FROM get_all_data();
--- Task3/5-Advanced-Functions.sql
+++ Task3/5-Advanced-Functions.sql
@@ -1,54 +1,3 @@
--- Display All Cars - fetch all data
-CREATE OR REPLACE FUNCTION get_all_data()
-    RETURNS TABLE (
-        brand_name        TEXT,
-        vehicle_type_name TEXT,
-        engine_type_name  TEXT,
-        model             TEXT,
-        production_year   TEXT,
-        price             TEXT
-    )
-    LANGUAGE plpgsql
-AS
-$$
-BEGIN
-    RETURN QUERY SELECT brand.brand_name, vehicle_type.vehicle_type_name,
-                        engine_type.engine_type_name, car.model, car.production_year,
-                        car.price
-                 FROM car_showroom_single_column,
-                      XMLTABLE('/car_showroom/brands/brand' PASSING xml_data
-                               COLUMNS
-                                   brand_id TEXT PATH '@brand_id' NOT NULL ,
-                                   brand_name TEXT PATH '.' NOT NULL
-                          ) brand,
-                      XMLTABLE('/car_showroom/vehicle_types/vehicle_type' PASSING xml_data
-                               COLUMNS
-                                   vehicle_type_id TEXT PATH '@vehicle_type_id' NOT NULL,
-                                   vehicle_type_name TEXT PATH '.' NOT NULL
-                          ) vehicle_type,
-                      XMLTABLE('/car_showroom/engine_types/engine_type' PASSING xml_data
-                               COLUMNS
-                                   engine_id TEXT PATH '@engine_id' NOT NULL,
-                                   engine_type_name TEXT PATH '.' NOT NULL
-                          ) engine_type,
-                      XMLTABLE('/car_showroom/cars/car' PASSING xml_data
-                               COLUMNS
-                                   brand_id TEXT PATH '@brand_id' NOT NULL,
-                                   vehicle_type_id TEXT PATH '@vehicle_type_id' NOT NULL,
-                                   engine_id TEXT PATH '@engine_id' NOT NULL,
-                                   model TEXT PATH 'model' NOT NULL,
-                                   production_year TEXT PATH 'production_year' NOT NULL,
-                                   price TEXT PATH 'price' NOT NULL
-                          ) car
-                 WHERE car.brand_id = brand.brand_id
-                   AND car.vehicle_type_id = vehicle_type.vehicle_type_id
-                   AND car.engine_id = engine_type.engine_id;
-END;
-$$;
-
-SELECT *
-FROM get_all_data();
-
 -- Display chosen cars based on brand name
 CREATE OR REPLACE FUNCTION get_cars_chosen_brand(chosen_brand_name TEXT)
     RETURNS TABLE (
@@ -77,6 +26,7 @@ $$;
 
 SELECT *
 FROM get_cars_chosen_brand('Audi');
+
 
 -- Display chosen cars based on vehicle type name
 CREATE OR REPLACE FUNCTION get_cars_chosen_vehicle_type(chosen_vehicle_type_name TEXT)
@@ -109,6 +59,7 @@ $$;
 
 SELECT *
 FROM get_cars_chosen_vehicle_type('SUV');
+
 
 -- Display chosen cars based on engine_type name
 CREATE OR REPLACE FUNCTION get_cars_chosen_engine_type(chosen_engine_type_name TEXT)
