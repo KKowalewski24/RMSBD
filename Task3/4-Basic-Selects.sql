0fc6a8c56076de442b7a9bba138fb2a5988c9c3d
Kamil Kowalewski <szamil24@gmail.com>
2021-03-18 21:42:40 +0100

    Add ids in selects

--- Task3/4-Basic-Selects.sql
+++ Task3/4-Basic-Selects.sql
@@ -7,6 +7,7 @@ SELECT xmltable.*
 FROM car_showroom_single_column,
     XMLTABLE('/car_showroom/brands/brand' PASSING xml_data
              COLUMNS
+                 brand_id TEXT PATH '@brand_id',
                  name TEXT PATH '.' NOT NULL
         );
 
@@ -15,6 +16,7 @@ SELECT xmltable.*
 FROM car_showroom_single_column,
     XMLTABLE('/car_showroom/vehicle_types/vehicle_type' PASSING xml_data
              COLUMNS
+                 vehicle_type_id TEXT PATH '@vehicle_type_id',
                  name TEXT PATH '.' NOT NULL
         );
 
@@ -23,17 +25,8 @@ SELECT xmltable.*
 FROM car_showroom_single_column,
     XMLTABLE('/car_showroom/engine_types/engine_type' PASSING xml_data
              COLUMNS
+                 engine_id TEXT PATH '@engine_id',
                  name TEXT PATH '.' NOT NULL
-        );
-
--- Display Cars
-SELECT xmltable.*
-FROM car_showroom_single_column,
-    XMLTABLE('/car_showroom/cars/car' PASSING xml_data
-             COLUMNS
-                 model TEXT PATH 'model'NOT NULL,
-                 production_year TEXT PATH 'production_year' NOT NULL,
-                 price TEXT PATH 'price' NOT NULL
         );
 
 -- Display All Data - only ID
