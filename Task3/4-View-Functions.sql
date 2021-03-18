6fcd79bb985e013e1dbaafbbaea5931ba3293ff1
Kamil Kowalewski <szamil24@gmail.com>
2021-03-18 21:16:56 +0100

    Add displaying all data

--- Task3/4-View-Functions.sql
+++ Task3/4-View-Functions.sql
@@ -1,9 +1,7 @@
 -- https://www.2ndquadrant.com/en/blog/xmltable-intro/
 -- https://www.depesz.com/2017/03/09/waiting-for-postgresql-10-support-xmltable-query-expression/
 
-SELECT xml_data
-FROM car_showroom_single_column;
-
+-- Display Brands
 SELECT xmltable.*
 FROM car_showroom_single_column,
     XMLTABLE('/car_showroom/brands/brand' PASSING xml_data
@@ -11,6 +9,7 @@ FROM car_showroom_single_column,
                  name TEXT PATH '.' NOT NULL
         );
 
+-- Display Vehicle Types
 SELECT xmltable.*
 FROM car_showroom_single_column,
     XMLTABLE('/car_showroom/vehicle_types/vehicle_type' PASSING xml_data
@@ -18,6 +17,7 @@ FROM car_showroom_single_column,
                  name TEXT PATH '.' NOT NULL
         );
 
+-- Display Engine Types
 SELECT xmltable.*
 FROM car_showroom_single_column,
     XMLTABLE('/car_showroom/engine_types/engine_type' PASSING xml_data
@@ -31,6 +31,19 @@ FROM car_showroom_single_column,
     XMLTABLE('/car_showroom/cars/car' PASSING xml_data
              COLUMNS
                  model TEXT PATH 'model'NOT NULL,
+                 production_year TEXT PATH 'production_year' NOT NULL,
+                 price TEXT PATH 'price' NOT NULL
+        );
+
+-- Display All Data
+SELECT xmltable.*
+FROM car_showroom_single_column,
+    XMLTABLE('/car_showroom/cars/car' PASSING xml_data
+             COLUMNS
+                 brand_id TEXT PATH '@brand_id',
+                 vehicle_type_id TEXT PATH '@vehicle_type_id',
+                 engine_id TEXT PATH '@engine_id',
+                 model TEXT PATH 'model' NOT NULL,
                  production_year TEXT PATH 'production_year' NOT NULL,
                  price TEXT PATH 'price' NOT NULL
         );
