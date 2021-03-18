7a546bb86bb183bf864e6435fb6051e4259c7215
Kamil Kowalewski <szamil24@gmail.com>
2021-03-18 21:10:46 +0100

    Add path in all places

--- Task3/4-View-Functions.sql
+++ Task3/4-View-Functions.sql
@@ -8,21 +8,21 @@ SELECT xmltable.*
 FROM car_showroom_single_column,
     XMLTABLE('/car_showroom/brands/brand' PASSING xml_data
              COLUMNS
-                 name TEXT NOT NULL
+                 name TEXT PATH '.' NOT NULL
         );
 
 SELECT xmltable.*
 FROM car_showroom_single_column,
     XMLTABLE('/car_showroom/vehicle_types/vehicle_type' PASSING xml_data
              COLUMNS
-                 name TEXT NOT NULL
+                 name TEXT PATH '.' NOT NULL
         );
 
 SELECT xmltable.*
 FROM car_showroom_single_column,
     XMLTABLE('/car_showroom/engine_types/engine_type' PASSING xml_data
              COLUMNS
-                 name TEXT NOT NULL
+                 name TEXT PATH '.' NOT NULL
         );
 
 -- Display Cars
@@ -31,6 +31,6 @@ FROM car_showroom_single_column,
     XMLTABLE('/car_showroom/cars/car' PASSING xml_data
              COLUMNS
                  model TEXT PATH 'model'NOT NULL,
-                 production_year TEXT NOT NULL,
-                 price TEXT NOT NULL
+                 production_year TEXT PATH 'production_year' NOT NULL,
+                 price TEXT PATH 'price' NOT NULL
         );
