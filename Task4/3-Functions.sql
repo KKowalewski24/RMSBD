0207af686d6e1f6339bf6b88be5460cc05c19702
Kamil Kowalewski <szamil24@gmail.com>
2021-03-20 13:13:03 +0100

    Add calculating distance between showrooms

--- Task4/3-Functions.sql
+++ Task4/3-Functions.sql
@@ -53,3 +53,52 @@ $$;
 
 SELECT *
 FROM get_chosen_showrooms(7);
+
+
+-- Calculates distance between two chosen showrooms
+CREATE OR REPLACE FUNCTION calculate_showrooms_distance(first_id INT, second_id INT)
+    RETURNS DOUBLE PRECISION
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    distance DOUBLE PRECISION;
+BEGIN
+    PERFORM * FROM showrooms WHERE id = first_id;
+
+    IF NOT found THEN
+        RAISE NOTICE 'Passed first_id does not exist!';
+        RETURN 0;
+    END IF;
+
+    PERFORM * FROM showrooms WHERE id = second_id;
+
+    IF NOT found THEN
+        RAISE NOTICE 'Passed second_id does not exist!';
+        RETURN 0;
+    END IF;
+
+    distance = st_distance(
+            (
+                SELECT location
+                FROM showrooms
+                WHERE id = first_id
+            ),
+            (
+                SELECT location
+                FROM showrooms
+                WHERE id = second_id
+            )
+        );
+
+    IF distance = 0 THEN
+        RAISE NOTICE 'distance = 0, cannot divide by ZERO!';
+        RETURN 0;
+    END IF;
+
+    -- Convert to KM
+    RETURN round(distance / 1000);
+END;
+$$;
+
+SELECT calculate_showrooms_distance(1, 7) AS distance;
