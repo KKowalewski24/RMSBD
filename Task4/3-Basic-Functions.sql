b476c345a93febaf8b958adb0a3a4ba9b1f0a636
Kamil Kowalewski <szamil24@gmail.com>
2021-03-20 10:32:11 +0100

    Add get_chosen_showrooms function

--- Task4/3-Basic-Functions.sql
+++ Task4/3-Basic-Functions.sql
@@ -23,3 +23,30 @@ $$;
 
 SELECT *
 FROM get_all_showrooms();
+
+-- Display chosen showroom
+CREATE OR REPLACE FUNCTION get_chosen_showrooms(chosen_id INT)
+    RETURNS TABLE (
+        id              INT,
+        name            TEXT,
+        city            TEXT,
+        street          TEXT,
+        building_number TEXT,
+        longitude       FLOAT,
+        latitude        FLOAT
+    )
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    RETURN QUERY
+        SELECT sr.id, sr.name, sr.city, sr.street, sr.building_number,
+               ST_X(sr.location:: GEOMETRY) AS longitude,
+               ST_Y(sr.location:: GEOMETRY) AS latitude
+        FROM showrooms sr
+        WHERE sr.id = chosen_id;
+END;
+$$;
+
+SELECT *
+FROM get_chosen_showrooms(7);
